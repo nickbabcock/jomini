9714dbc71ee5c8206cb44445d74ff076e0d11a84
Nick Babcock <nbabcock19@hotmail.com>
2015-01-29 20:52:59 -0500

    Add header implementation

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('./lib/para');
+exports = module.exports = require('./lib/para');
+exports.Header = require('./lib/header');
---
+++ test/header.js
@@ -0,0 +1,29 @@
+var Header = require('../').Header;
+var stream = require('stream');
+var expect = require('chai').expect
+
+describe('Header', function() {
+    it('detect expected header', function(done) {
+        var s = new stream.Readable();
+        s.push("EU4txtblah");
+        s.push(null);
+        var head = new Header({ header: 'EU4txt'});
+        s.pipe(head);
+        head.on('data', function(data) {
+            expect(data.toString()).to.equal('blah');
+            done();
+        });
+    });
+
+    it('error on unexpected header', function(done) {
+        var s = new stream.Readable();
+        s.push("EU4binblah");
+        s.push(null);
+        var head = new Header({ header: 'EU4txt'});
+        s.pipe(head);
+        head.on('error', function(err) {
+            expect(err.message).to.equal('Expected EU4txt but received EU4bin');
+            done();
+        });
+    });
+});
