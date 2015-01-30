8932ed01d17ee44dc1e39bcfd0a82390addf0010
Nick Babcock <nbabcock19@hotmail.com>
2015-01-30 11:08:06 -0500

    Add istanbul coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
-gulp.task('test', function() {
-    return gulp.src(['test/*js']).pipe(jscs({
-        preset: 'google',
-        requireCamelCaseOrUpperCaseIdentifiers: 'ignoreProperties'
-    })).pipe(mocha());
+gulp.task('test', function(cb) {
+    gulp.src('lib/*.js')
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function() {
+            gulp.src(['test/*js'])
+            .pipe(jscs({
+                preset: 'google',
+                requireCamelCaseOrUpperCaseIdentifiers: 'ignoreProperties'
+            }))
+            .pipe(mocha())
+            .pipe(istanbul.writeReports())
+            .on('end', cb);
+        });
 });
 
 gulp.task('main', ['test'], function() {
