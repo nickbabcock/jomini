c353eba01a8a063a08809b32add138ca966e5204
Nick Babcock <nbabcock19@hotmail.com>
2015-01-30 23:05:07 -0500

    2 space indent for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,32 @@ var jshint = require('gulp-jshint');
 var istanbul = require('gulp-istanbul');
 
 gulp.task('test', function(cb) {
-    gulp.src('lib/*.js')
-        .pipe(istanbul())
-        .pipe(istanbul.hookRequire())
-        .on('finish', function() {
-            gulp.src(['test/*js'])
-            .pipe(jscs({
-                preset: 'google',
-                requireCamelCaseOrUpperCaseIdentifiers: 'ignoreProperties'
-            }))
-            .pipe(mocha())
-            .pipe(istanbul.writeReports())
-            .on('end', cb);
-        });
+  gulp.src('lib/*.js')
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function() {
+      gulp.src(['test/*js'])
+      .pipe(jscs({
+        preset: 'google',
+        requireCamelCaseOrUpperCaseIdentifiers: 'ignoreProperties'
+      }))
+      .pipe(mocha())
+      .pipe(istanbul.writeReports())
+      .on('end', cb);
+    });
 });
 
 gulp.task('lint', function() {
-    return gulp.src(['lib/**/*.js', 'test/**/*.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(jshint.reporter('fail'));
+  return gulp.src(['lib/**/*.js', 'test/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'));
 });
 
 gulp.task('main', ['test', 'lint'], function() {
-    return gulp.src(['lib/*js']).pipe(jscs({
-        preset: 'google' 
-    }));
+  return gulp.src(['lib/*js']).pipe(jscs({
+    preset: 'google'
+  }));
 });
 
 gulp.task('default', ['main']);
