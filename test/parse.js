019c913067b4a678b6d5bfeabec080902921352c
Nick Babcock <nbabcock19@hotmail.com>
2015-02-01 08:50:04 -0500

    Show that the parse function can be promisified

--- test/parse.js
+++ test/parse.js
@@ -1,10 +1,19 @@
 var parse = require('../').parse;
 var expect = require('chai').expect;
+var Promise = require('bluebird');
 
 describe('parse', function() {
   it('should handle the simple parse case', function(done) {
     parse('foo=bar', function(err, actual) {
       expect(actual).to.deep.equal({'foo': 'bar'});
+      done();
+    });
+  });
+
+  it('should be able to be promisified', function(done) {
+    var parseAsync = Promise.promisify(parse);
+    parseAsync('foo=bar').then(function(data) {
+      expect(data).to.deep.equal({'foo': 'bar'});
       done();
     });
   });
