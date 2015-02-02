3fbf77d64c441ae33f727b9987a9535e2f160a2f
Nick Babcock <nbabcock19@hotmail.com>
2015-02-02 09:25:27 -0500

    Add ability to parse HOI style dates

--- test/toDate.js
+++ test/toDate.js
@@ -6,4 +6,10 @@ describe('toDate', function() {
     var actual = toDate('1800.1.1');
     expect(actual).to.deep.equal(new Date(Date.UTC(1800, 0, 1)));
   });
+
+  it('should parse hour dates', function() {
+    var actual = toDate('1800.1.1.2');
+    var expected = new Date(Date.UTC(1800, 0, 1, 2));
+    expect(actual).to.deep.equal(expected);
+  });
 });
