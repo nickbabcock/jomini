8c28d07ef6baf2b2bbabb745c140dcb438e90d10
Nick Babcock <nbabcock19@hotmail.com>
2015-02-03 20:43:54 -0500

    Use a better JSON library

--- cli.js
+++ cli.js
@@ -1,7 +1,8 @@
 var jomini = require('./');
+var JS = require('json3');
 var header = new jomini.Header({header: 'EU4txt'});
 var parser = new jomini.Parser();
 process.stdin.pipe(header).pipe(parser);
 parser.on('finish', function() {
-  process.stdout.write(JSON.parse(parser.obj), 'utf8', function() {});
+  process.stdout.write(JS.stringify(parser.obj), 'utf8', function() {});
 });
