586ed7ada4ddb6442b7839f54a7c29fb3a7e6674
Nick Babcock <nbabcock19@hotmail.com>
2015-02-05 16:35:23 -0500

    Fix commandline useage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports = module.exports;
-exports.parse = require('./lib/jomini').parser.parse;
+exports.parse = require('./lib/jomini').parse;
 exports.toArray = require('./lib/toArray');
 exports.toDate = require('./lib/toDate');
 exports.toBool = require('./lib/toBool');	
