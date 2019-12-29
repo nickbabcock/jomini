de2cc1600f2db512aed8d6c4319d8e197e0634e3
Ivan Rubinson <soryy708@gmail.com>
2019-12-29 22:46:47 +0200

    Remove json3 dependency https://github.com/nickbabcock/jomini/issues/14

--- cli.js
+++ cli.js
@@ -1,10 +1,9 @@
 var jomini = require('./');
-var JS = require('json3');
 var concat = require('concat-stream');
 var concatStream = concat(function(buf) {
     var str = buf.toString('utf8');
     var obj = jomini.parse(str);
-    process.stdout.write(JS.stringify(obj));
+    process.stdout.write(JSON.stringify(obj));
 });
 
 process.stdin.pipe(concatStream)
